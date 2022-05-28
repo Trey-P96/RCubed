9ce4d6b88f3702be6be97a1ae3586e97160435a0
TreyPopp <74880416+TreyPopp@users.noreply.github.com>
2022-05-28 14:37:14 -0600

    home page initialized

---
+++ lib/pages/home/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+  
+}
