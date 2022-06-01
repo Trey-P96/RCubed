d449b7a5e1b00d997daeb17a08ef50d675e49ab9
TreyPopp <74880416+TreyPopp@users.noreply.github.com>
2022-06-01 17:17:09 -0600

    debugging

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
 import 'package:rcubed/content/home_content.dart';
 import 'package:flutter/material.dart';
 import 'package:rcubed/widgets/navigation_bar/nav_bar.dart';
@@ -6,28 +10,27 @@ import 'package:flutter_svg/svg.dart';
 import '../../themes/theme.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final ScrollController controller = ScrollController();
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const String logoPath = 'assets/images/RcubedLogo.svg';
-    final Widget logo = SvgPicture.asset(
-      logoPath,
-      color: MyTheme().primary,
-      width: 200,
-    );
+
+    // const String logoPath = 'assets/images/RcubedLogo.svg';
+    // final Widget logo = SvgPicture.asset(
+    //   logoPath,
+    //   color: MyTheme().primary,
+    //   width: 200,
+    // );
     // TODO: implement build
     return Column(
       children: [
         NavBar(),
+        
         Expanded(
-          child: ListView.builder(
-            itemCount: 5,
-            itemBuilder: (c, i) => Padding(
-              padding: EdgeInsets.only(top: 40),
-              child: logo,
-            ),
-          ),
+          child: SingleChildScrollView(
+                  child: Text(HomeContent().diem),
+                ),
         ),
       ],
     );
