88bf971631fbf36afd9ecafab366d507c6b04766
TreyPopp <74880416+TreyPopp@users.noreply.github.com>
2022-06-04 11:54:48 -0600

    scrolling update fix?

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:collection/collection.dart';
-
+import 'dart:math';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/src/physics/spring_simulation.dart';
@@ -21,18 +21,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // MyQ q = MyQ();
     List<double> q = [];
-    controller.addListener(() {
-      double overscroll = 250;
-      if(controller.offset > controller.position.maxScrollExtent + overscroll){
-        controller.position.activity!.resetActivity();
-        controller.jumpTo(controller.position.maxScrollExtent+overscroll);
-      }
-      if(controller.offset < controller.position.minScrollExtent -overscroll){
-        controller.position.activity!.resetActivity();
-        controller.jumpTo(controller.position.minScrollExtent-overscroll);
-      }
-    });
-    //print(controller.position.maxScrollExtent);
+    // controller.addListener(() {
+    //   double overscroll = 250;
+    //
+    //
+    //   if(controller.offset > controller.position.maxScrollExtent + overscroll){
+    //     controller.position.activity!.delegate.goIdle();
+    //     controller.position.activity!.resetActivity();
+    //     controller.position.jumpTo(controller.position.maxScrollExtent+overscroll);
+    //   }
+    //   if(controller.offset < controller.position.minScrollExtent -overscroll){
+    //     controller.position.activity!.delegate.goIdle();
+    //     controller.position.activity!.resetActivity();
+    //     controller.position.jumpTo(controller.position.minScrollExtent-overscroll);
+    //   }
+    // });
+
 
     // const String logoPath = 'assets/images/RcubedLogo.svg';
     // final Widget logo = SvgPicture.asset(
@@ -47,32 +51,68 @@ class HomePage extends StatelessWidget {
         Expanded(
           child: Listener(
             onPointerMove: (details){
-              controller.jumpTo(controller.offset+(-details.delta.dy));
+              if(!controller.position.outOfRange){
+                controller.jumpTo(controller.offset+(-details.delta.dy));
+              }
+
+            },
+            onPointerUp: (details){
             },
             onPointerSignal: (ps) {
-              // if(ps is PointerScrollEvent){
-              //   controller.position.moveTo(controller.offset+ps.scrollDelta.dy, duration: Duration(milliseconds: 150),curve: Curves.linearToEaseOut, clamp: false);
-              //   controller.position.activity!.resetActivity();
-              // }
-
               if (ps is PointerScrollEvent) {
                 q.add(ps.scrollDelta.dy.abs());
                 if(q.length > 10) q.removeRange(3, q.length-1);
                 if(q.average != ps.scrollDelta.dy.abs()) controller.jumpTo(controller.offset+ps.scrollDelta.dy);
-                else controller.animateTo(controller.offset+ps.scrollDelta.dy, duration: Duration(milliseconds: 200), curve: Curves.linearToEaseOut);
+                else controller.animateTo(controller.offset+ps.scrollDelta.dy, duration: Duration(milliseconds: 250), curve: Curves.linearToEaseOut);
               }
             },
-            child: SingleChildScrollView(
-              controller: controller,
-              // physics: NeverScrollableScrollPhysics().applyTo(BouncingScrollPhysics()),
-              physics: NeverScrollableScrollPhysics().applyTo(BouncingScrollPhysics()),
-              child: Text(HomeContent().diem),
+            child: GestureDetector(
+              onPanEnd: (details){
+                double velocity = details.velocity.pixelsPerSecond.dy/10;
+                controller.animateTo(controller.offset-velocity, duration: Duration(milliseconds: 500), curve: Curves.linearToEaseOut);
+              },
+              child: SingleChildScrollView(
+                controller: controller,
+                physics: ClampingScrollPhysics().applyTo(NeverScrollableScrollPhysics()),
+                // physics: CustomScrollPhysics(parent: NeverScrollableScrollPhysics()),
+                child: Text(HomeContent().diem),
+              ),
             ),
           ),
         ),
       ],
     );
   }
+}
+
+
+
+class CustomScrollPhysics extends ScrollPhysics {
+  const CustomScrollPhysics({required ScrollPhysics parent})
+      : super(parent: parent);
+
+  @override
+  double get minFlingVelocity => 100;
+
+  @override
+  double get maxFlingVelocity => 100;
+
+  @override
+  double get minFlingDistance => double.infinity;
+
+  @override
+  double carriedMomentum(double existingVelocity) {
+    if (parent == null)
+      return 0.0;
+    return parent!.carriedMomentum(existingVelocity);
+  }
+
+  Simulation? createBallisticSimulation(ScrollMetrics position, double velocity) {
+    if (parent == null)
+      return null;
+    return parent!.createBallisticSimulation(position, velocity);
+  }
+
 }
 
 
