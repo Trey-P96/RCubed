50bea903cbaa3970b7555721e9591cc5d54fd571
TreyPopp <74880416+TreyPopp@users.noreply.github.com>
2022-06-07 11:02:02 -0600

    add scrollwheel animations

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      scrollBehavior: CupertinoScrollBehavior(),
+      scrollBehavior: MaterialScrollBehavior(),
       debugShowCheckedModeBanner: false,
       title: 'R-Cubed Consulting',
       theme: ThemeData(
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,9 +1,10 @@
+import 'dart:ui';
+import 'package:collection/collection.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_builder/responsive_builder.dart';
-import 'home_desktop.dart';
-import 'home_mobile.dart';
-import 'home_tablet.dart';
-
+import 'package:rcubed/widgets/rcubed_logo/rcubed_logo.dart';
+import '../../widgets/navigation_bar/nav_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,79 +12,82 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ScreenTypeLayout(
-      desktop: const HomeDesktop(),
-      mobile: const HomeMobile(),
-      tablet: const HomeTablet(),
+    return Column(
+      children: [
+        NavBar(),
+        const AdaptiveScroll(),
+      ],
     );
   }
 }
 
-// class Handler extends StatelessWidget{
-//
-//   final ScrollController controller;
-//   List<double> q = [];
-//   final Widget child;
-//   var scrollPhysics = ScrollPhysics();
-//   double velocity = 0;
-//
-//   Handler(this.controller, this.child,{Key? key}) : super(key: key){
-//     _start();
-//   }
-//
-//   void _start(){
-//     // Timer.periodic(Duration(milliseconds: 250), (timer) {
-//     //   if(controller.hasClients){
-//     //     velocity = controller.offset-initial;
-//     //     initial = controller.offset;
-//     //   }
-//     // });
-//     controller.addListener(() {
-//       double maxBoundary = controller.position.maxScrollExtent;
-//       double minBoundary = controller.position.minScrollExtent;
-//       if(controller.offset > maxBoundary){
-//         controller.jumpTo(controller.position.maxScrollExtent);
-//       }
-//       if(controller.offset < minBoundary){
-//         controller.jumpTo(controller.position.minScrollExtent);
-//       }
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     return Listener(
-//       onPointerUp: (details){
-//
-//       },
-//       onPointerMove: (details){
-//         if(!controller.position.outOfRange){
-//           controller.jumpTo(controller.offset-details.delta.dy);
-//         }
-//       },
-//       onPointerSignal: (ps){
-//         if (ps is PointerScrollEvent) {
-//           q.add(ps.scrollDelta.dy.abs());
-//           if(q.length > 10) q.removeRange(3, q.length-1);
-//           if(q.average != ps.scrollDelta.dy.abs() && !controller.position.outOfRange) {
-//             controller.jumpTo(controller.offset+ps.scrollDelta.dy);
-//           } else {
-//             controller.animateTo(controller.offset+ps.scrollDelta.dy, duration: const Duration(milliseconds: 175), curve: Curves.linearToEaseOut);
-//           }
-//         }
-//       },
-//       child: GestureDetector(
-//         onPanStart: (details){
-//
-//         },
-//         onPanEnd: (details){
-//           double velocity = details.velocity.pixelsPerSecond.dy/5;
-//           controller.animateTo(controller.offset-velocity, duration: Duration(milliseconds: 800), curve: Curves.decelerate);
-//         },
-//         child: child,
-//       ),
-//     );
-//   }
-//
-// }
+class AdaptiveScroll extends StatefulWidget {
+  const AdaptiveScroll({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AdaptiveScrollState();
+  }
+}
+
+class AdaptiveScrollState extends State<AdaptiveScroll> {
+  final controller = ScrollController();
+  ScrollPhysics scrollPhysics = const AlwaysScrollableScrollPhysics();
+  PointerDeviceKind device = PointerDeviceKind.touch;
+  List<double> q = [];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Expanded(
+      child: Listener(
+        onPointerDown: (pointer) {
+          if (pointer.kind == PointerDeviceKind.touch &&
+              device != pointer.kind) {
+            setState(() {
+              device = pointer
+                  .kind; // set to avoid extraneous setState calls after device is detected.
+              scrollPhysics = const AlwaysScrollableScrollPhysics();
+            });
+          }
+        },
+        onPointerHover: (pointer) {
+          if (pointer.kind == PointerDeviceKind.mouse &&
+              device != pointer.kind) {
+            setState(() {
+              device = pointer
+                  .kind; // set to avoid extraneous setState calls after device is detected.
+              scrollPhysics = const NeverScrollableScrollPhysics();
+            });
+          }
+        },
+        onPointerSignal: (ps) {
+          if (ps is PointerScrollEvent) {
+            q.add(ps.scrollDelta.dy.abs());
+            if (q.length > 10) q.removeRange(3, q.length - 1);
+            if (q.average != ps.scrollDelta.dy.abs() &&
+                !controller.position.outOfRange) {
+              controller.jumpTo(controller.offset + ps.scrollDelta.dy);
+            } else {
+              controller.animateTo(controller.offset + ps.scrollDelta.dy,
+                  duration: const Duration(milliseconds: 250),
+                  curve: Curves.easeOutQuart);
+            }
+          }
+        },
+        child: ListView.builder(
+            controller: controller,
+            physics: scrollPhysics,
+            shrinkWrap: true,
+            itemCount: 30,
+            itemBuilder: (context, i) {
+              return const Padding(
+                padding: EdgeInsets.all(10),
+                child: Logo(200),
+              );
+            }),
+      ),
+    );
+  }
+}
