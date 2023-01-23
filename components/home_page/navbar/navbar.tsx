f4f8bd3a5cbac02c64743552abb7a866f22ec3da
TreyPopp <74880416+TreyPopp@users.noreply.github.com>
2023-01-22 20:21:06 -0500

    ...

--- components/home_page/navbar/navbar.tsx
+++ components/home_page/navbar/navbar.tsx
@@ -1,9 +1,14 @@
 import styles from "./navbar.module.css"
+import Logo from "/public/svgs/icons/menu.svg"
+
 
 export default function Navbar(){
     return(
-        <div className={styles.navbar}>
-            <p>Navbar</p>
+        <div className={styles.banner}>
+            <section className={styles.navbar}>
+                <img src="/svgs/logo_white.svg" alt="Logo" className={styles.navbarItem} />
+                <Logo className={styles.navbarItem}></Logo>
+            </section>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from '../components/home_page/navbar/navbar'
+// import Navbar from '../components/home_page/navbar/navbar'
+import Navbar from '/components/home_page/navbar/navbar'
 import WhatWeDo from '@/components/home_page/what_we_do/what_we_do'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,10 +16,12 @@ export default function Home() {
         <meta name="description" content="Relationships Results Reputation" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-        <Navbar></Navbar>
+      </Head>       
         <main>
-          <WhatWeDo></WhatWeDo>
+          <Navbar></Navbar>
+            <section>
+              <WhatWeDo></WhatWeDo>
+            </section>
         </main>
       
     </>
