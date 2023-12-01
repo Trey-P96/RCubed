0615affcc40944a25759fc255a9ec26f5d490749
Trey Popp <trey.popp@rcubedco.com>
2023-12-01 13:46:35 -0500

    update documentation

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ void main() {
    */
 
   //Step 3 (for staging): update href inside build/web/index.html on line 17 to "/staged/" (assuming "staged" folder exists under /root) .. for production: set <base href="/">
+  //Step 3.1: inside build/web/index.html replace "A new Flutter project" inside <meta name="description" content="A new Flutter project.">
   //Step 4: copy code from index.html and paste into 404.html. If 404.html does not exist inside build folder, then create it manually in same folder as index.html... ps. if in staged, manually typing url in url bar (eg. rcubedco/staged/contact) will result in 404 page not found.
   //Step 5: under project folder navigate to folder "icons" and copy all files EXCEPT for favicon.png, then paste and overwrite into build/web/icons. Then copy and overwrite favicon.png to build/web
   //Step 6: inside build/web/ replace favicon.png with favicon.png located in RCubed/icons.
